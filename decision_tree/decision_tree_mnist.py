5bbfaf7bced5a891edd2d90d3f75d7dff3d1b39c
Clearlove4396 <969595748@qq.com>
2019-07-30 11:19:09 +0800

    add test function

--- decision_tree/decision_tree_mnist.py
+++ decision_tree/decision_tree_mnist.py
@@ -8,19 +8,26 @@ import numpy as np
 def readDataSet(fileDir):
     fileList = listdir(fileDir)
     m = len(fileList)
-    dataSet = np.zeros((m, 1025))
+ #   dataSet = np.zeros((m, 1025))
+    dataSet = []
     index = 0
     for fileName in fileList:
         fr = open(fileDir + "/" + fileName)
+        data = []
         for i in range(32):
             line = fr.readline()
             for j in range(32):
-                dataSet[index][32 * i + j] = int(line[j])
+                data.append(int(line[j]))
+        dataSet.append(data)
         dataLabel = int((fileName.split('.')[0]).split('_')[0])
         dataSet[index][-1] = str(dataLabel)
         index += 1
-
-    return dataSet
+    featureNamesSet = []
+    for i in range(len(dataSet[0]) - 1):
+        col = [x[i] for x in dataSet]
+        colSet = set(col)
+        featureNamesSet.append(list(colSet))
+    return dataSet, featureNamesSet
 
 #计算熵
 def calcEntropy(dataSet):
@@ -41,12 +48,11 @@ def calcEntropy(dataSet):
 # # 返回值 - dataSet中index所在特征为feature，且去掉index一列的集合
 def splitDataSet(dataSet, index, feature):
     splitedDataSet = []
-    mD = len(dataSet)
     for data in dataSet:
         if(data[index] == feature):
-            sliceTmp = data[:index]
-            sliceTmp.extend(data[index + 1:])
-            splitedDataSet.append(sliceTmp)
+            reduceFeatureVec = data[: index]
+            reduceFeatureVec.extend(data[index + 1 :])
+            splitedDataSet.append(reduceFeatureVec)
     return splitedDataSet
 
 #根据信息增益 - 选择最好的特征
@@ -71,7 +77,6 @@ def chooseBestFeature(dataSet):
         elif(maxGain < entDCopy):
             maxGain = entDCopy
             maxIndex = i
-
     return maxIndex
 
 # 寻找最多的，作为标签
@@ -110,15 +115,14 @@ def mainLabel(labelList):
 def createFullDecisionTree(dataSet, featureNames, featureNamesSet, labelListParent):
     labelList = [x[-1] for x in dataSet]
     if(len(dataSet) == 0):
-        print("2222")
         return mainLabel(labelListParent)
     elif(len(dataSet[0]) == 1): #没有可划分的属性了
-        print("3333")
         return mainLabel(labelList)  #选出最多的label作为该数据集的标签
     elif(labelList.count(labelList[0]) == len(labelList)): # 全部都属于同一个Label
         return labelList[0]
 
     bestFeatureIndex = chooseBestFeature(dataSet)
+    print(bestFeatureIndex)
     bestFeatureName = featureNames.pop(bestFeatureIndex)
     myTree = {bestFeatureName: {}}
     featureList = featureNamesSet.pop(bestFeatureIndex)
@@ -142,7 +146,6 @@ def readWatermelonDataSet():
         tmp = line.split('\n')[0]
         tmp = tmp.split(',')
         dataSet.append(tmp)
-
     #获取featureNamesSet
     featureNamesSet = []
     for i in range(len(dataSet[0]) - 1):
@@ -152,15 +155,105 @@ def readWatermelonDataSet():
 
     return dataSet, featureNames, featureNamesSet
 
+#  把决策树保存到文件中
+def saveMyTree(myTree, filename):
+    s = str(myTree)
+    ofile = open(filename, 'w')
+    ofile.writelines(s)
+    ofile.close()
+
+#  加载决策树
+def loadMyTree(filename):
+    # TODO: 异常检测
+    ifile = open(filename, 'r')
+    s = ifile.readline()
+    myTree = eval(s)   # eval() 函数用来执行一个字符串表达式，并返回表达式的值。
+                        #eval('pow(2, 3)')   8
+    return myTree
+
+#  test  全局变量法
+# testLabel = -1
+# def getLabel(myTree, data):
+#     global testLabel  #强调他是全局变量
+#     keys = myTree.keys()
+#     for key in keys:
+#         subTree = myTree[key]
+#         index = int(key)
+#         feature = int(data[index])
+#         nextTree = subTree[feature]
+#         if(type(nextTree).__name__ == 'dict'):
+#             getLabel(nextTree, data)
+#         else:
+#             if(testLabel == -1):
+#                 testLabel = int(nextTree)
+#
+# def test(myTree, testDataSet):
+#     errorCount = 0
+#     m = len(testDataSet)
+#     for data in testDataSet:
+#         global testLabel
+#         testLabel = -1
+#         realLabel = int(data[-1])
+#         getLabel(myTree, data[:-1])
+#
+#         if(testLabel == realLabel):
+#             print("right! label: %d" % realLabel)
+#         else:
+#             errorCount += 1
+#             print("error! realLabel: %d  forecastLabel: %d" % (realLabel, testLabel))
+#     print("total number: %d, errorRate: %f" % (m, float(errorCount) / m))
+
+
+#   测试
+def getLabel(myTree, data):
+    keys = myTree.keys()
+    for key in keys:
+        subTree = myTree[key]
+        index = int(key)
+        feature = int(data[index])
+        nextTree = subTree[feature]
+        if(type(nextTree).__name__ == 'dict'):
+            classLabel = getLabel(nextTree, data)
+        else:
+            classLabel = int(nextTree)
+    return classLabel
+
+def test(myTree, testDataSet):
+    errorCount = 0
+    m = len(testDataSet)
+    for data in testDataSet:
+        realLabel = int(data[-1])
+        testLabel = getLabel(myTree, data[:-1])
+
+        if(testLabel == realLabel):
+            print("right! label: %d" % realLabel)
+        else:
+            errorCount += 1
+            print("error! realLabel: %d  forecastLabel: %d" % (realLabel, testLabel))
+    print("total number: %d, errorRate: %f" % (m, float(errorCount) / m))
+
 def main():
-    #读取数据
-    trainingDataSet = readDataSet("data/trainingDigits")
-    featureNames = [str(x) for x in range(1024)]
+    # #读取数据
+    # trainingDataSet, featureNamesSet = readDataSet("data/trainingDigits")
+    # featureNames = [str(x) for x in range(1024)]
+    #
+    # #  训练 - 得到决策树myTree
+    # myTree = createFullDecisionTree(trainingDataSet, featureNames, featureNamesSet, [])
+    # #  保存到文件中
+    # saveMyTree(myTree, "myTree.txt")
+
+    myTree = loadMyTree("myTree.txt")
+    #读取测试数据
+    testDataSet, featureNamesSet = readDataSet("data/testDigits")
+    test(myTree, testDataSet)
 
 
-    dataSet, featureNames, featureNamesSet = readWatermelonDataSet()
-    print(createFullDecisionTree(dataSet, featureNames, featureNamesSet, []))
-    #print(createTree(dataSet, labels))
+    #  西瓜数据集
+    # dataSet, featureNames, featureNamesSet = readWatermelonDataSet()
+    # myTree = createFullDecisionTree(dataSet, featureNames, featureNamesSet, [])
+    # saveMyTree(myTree, "myTree.txt")
+    # a = loadMyTree("myTree.txt")
+    # print(a)
 
 if __name__ == "__main__":
     main()
